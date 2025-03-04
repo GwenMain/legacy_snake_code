eefccf8212812e2b45ab0098c09d0aa4582bad7e
m_chudej <m_chudej@utb.cz>
2025-03-04 12:51:55 +0100

    Rollback

--- legacy_snake_code/Program.cs
+++ legacy_snake_code/Program.cs
@@ -13,36 +13,24 @@ namespace Snake
 
         static void Main()
         {
-            GameEngine game = new GameEngine(WindowWidth, WindowHeight, InitialScore, MoveDelayMs);
-            game.Run();
-        }
-    }
-
-    class GameEngine
-    {
-        private readonly int _windowWidth;
-        private readonly int _windowHeight;
-        private readonly int _initialScore;
-        private readonly int _moveDelayMs;
-        private Renderer _renderer;
-        private InputHandler _inputHandler;
-
-        public GameEngine(int width, int height, int initialScore, int moveDelayMs)
-        {
-            _windowWidth = width;
-            _windowHeight = height;
-            _initialScore = initialScore;
-            _moveDelayMs = moveDelayMs;
-            _renderer = new ConsoleRenderer(width, height);
-            _inputHandler = new ConsoleInputHandler();
+            ConsoleSetup();
+            RunGame();
         }
 
-        public void Run()
+        private static void ConsoleSetup()
         {
+            Console.WindowHeight = WindowHeight;
+            Console.WindowWidth = WindowWidth;
+        }
+
+        private static void RunGame()
+        {
+            Console.CursorVisible = false;
+
             var random = new Random();
-            int score = _initialScore;
+            int score = InitialScore;
 
-            var head = new Pixel(_windowWidth / 2, _windowHeight / 2, ConsoleColor.Red);
+            var head = new Pixel(WindowWidth / 2, WindowHeight / 2, ConsoleColor.Red);
             var fruit = GenerateFruit(random);
             var body = new List<Pixel>();
             var currentDirection = Direction.Right;
@@ -50,8 +38,8 @@ namespace Snake
 
             while (!gameover)
             {
-                _renderer.Clear();
-                _renderer.DrawBorder();
+                Console.Clear();
+                DrawBorder();
 
                 if (CheckCollision(head))
                 {
@@ -64,15 +52,16 @@ namespace Snake
                     score++;
                 }
 
-                _renderer.DrawSnake(body, ref gameover, head);
+                DrawSnake(body, ref gameover, head);
                 if (gameover) break;
 
-                _renderer.DrawPixel(head);
-                _renderer.DrawPixel(new Pixel(fruit.XPos, fruit.YPos, fruit.Color));
+                DrawPixel(head);
+                fruit.Draw();
 
                 DelayMovement(ref currentDirection);
+
                 body.Add(new Pixel(head.X, head.Y, ConsoleColor.Green));
-                head = MoveHead(head, currentDirection);
+                MoveHead(ref head, currentDirection);
 
                 if (body.Count > score)
                 {
@@ -80,13 +69,14 @@ namespace Snake
                 }
             }
 
-            _renderer.DisplayGameOver(score - _initialScore);
+            DisplayGameOver(score);
+            Console.CursorVisible = true;
         }
 
-        private bool CheckCollision(Pixel head) =>
-            head.X == _windowWidth - 1 || head.X == 0 || head.Y == _windowHeight - 1 || head.Y == 0;
+        private static bool CheckCollision(Pixel head) =>
+            head.X == WindowWidth - 1 || head.X == 0 || head.Y == WindowHeight - 1 || head.Y == 0;
 
-        private bool CheckFruitCollision(ref Pixel head, ref Fruit fruit, Random random)
+        private static bool CheckFruitCollision(ref Pixel head, ref Fruit fruit, Random random)
         {
             if (fruit.XPos == head.X && fruit.YPos == head.Y)
             {
@@ -96,20 +86,88 @@ namespace Snake
             return false;
         }
 
-        private Fruit GenerateFruit(Random random) =>
-            new Fruit(random.Next(1, _windowWidth - 2), random.Next(1, _windowHeight - 2), ConsoleColor.DarkYellow);
+        private static Fruit GenerateFruit(Random random) =>
+     new Fruit(random.Next(1, WindowWidth - 2), random.Next(1, WindowHeight - 2), ConsoleColor.DarkYellow);
 
-        private void DelayMovement(ref Direction direction)
+
+
+        private static void DrawSnake(List<Pixel> body, ref bool gameover, Pixel head)
         {
-            var stopwatch = Stopwatch.StartNew();
-            while (stopwatch.ElapsedMilliseconds <= _moveDelayMs)
+            foreach (var segment in body)
             {
-                direction = _inputHandler.ReadMovement(direction);
+                DrawPixel(segment);
+                if (segment.X == head.X && segment.Y == head.Y)
+                {
+                    gameover = true;
+                }
             }
         }
 
-        private Pixel MoveHead(Pixel head, Direction direction) =>
-            direction switch
+        private static void DelayMovement(ref Direction direction)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            while (stopwatch.ElapsedMilliseconds <= MoveDelayMs)
+            {
+                direction = ReadMovement(direction);
+            }
+        }
+
+        private static void DisplayGameOver(int score)
+        {
+            Console.SetCursorPosition(WindowWidth / 5, WindowHeight / 2);
+            Console.WriteLine($"Game Over! Score: {score - InitialScore}");
+            Console.SetCursorPosition(WindowWidth / 5, WindowHeight / 2 + 1);
+            Console.ReadKey();
+        }
+
+        private static Direction ReadMovement(Direction currentDirection)
+        {
+            if (Console.KeyAvailable)
+            {
+                var key = Console.ReadKey(true).Key;
+                currentDirection = key switch
+                {
+                    ConsoleKey.UpArrow when currentDirection != Direction.Down => Direction.Up,
+                    ConsoleKey.DownArrow when currentDirection != Direction.Up => Direction.Down,
+                    ConsoleKey.LeftArrow when currentDirection != Direction.Right => Direction.Left,
+                    ConsoleKey.RightArrow when currentDirection != Direction.Left => Direction.Right,
+                    _ => currentDirection
+                };
+            }
+            return currentDirection;
+        }
+
+        private static void DrawPixel(Pixel pixel)
+        {
+            Console.SetCursorPosition(pixel.X, pixel.Y);
+            Console.ForegroundColor = pixel.Color;
+            Console.Write("■");
+            Console.SetCursorPosition(0, 0);
+        }
+
+        private static void DrawBorder()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;  // Ensure border is always white
+            for (int i = 0; i < WindowWidth; i++)
+            {
+                Console.SetCursorPosition(i, 0);
+                Console.Write("■");
+                Console.SetCursorPosition(i, WindowHeight - 1);
+                Console.Write("■");
+            }
+            for (int i = 0; i < WindowHeight; i++)
+            {
+                Console.SetCursorPosition(0, i);
+                Console.Write("■");
+                Console.SetCursorPosition(WindowWidth - 1, i);
+                Console.Write("■");
+            }
+        }
+
+
+        private static void MoveHead(ref Pixel head, Direction direction)
+        {
+            head = direction switch
             {
                 Direction.Up => new Pixel(head.X, head.Y - 1, head.Color),
                 Direction.Down => new Pixel(head.X, head.Y + 1, head.Color),
@@ -117,6 +175,7 @@ namespace Snake
                 Direction.Right => new Pixel(head.X + 1, head.Y, head.Color),
                 _ => head
             };
+        }
     }
 
     class Fruit
@@ -131,105 +190,34 @@ namespace Snake
             YPos = yPos;
             Color = color;
         }
-    }
 
-    interface Renderer
-    {
-        void Clear();
-        void DrawBorder();
-        void DrawPixel(Pixel pixel);
-        void DrawSnake(List<Pixel> body, ref bool gameover, Pixel head);
-        void DisplayGameOver(int score);
-    }
-
-    class ConsoleRenderer : Renderer
-    {
-        private int _width;
-        private int _height;
-
-        public ConsoleRenderer(int width, int height)
+        public void Draw()
         {
-            _width = width;
-            _height = height;
-            Console.WindowHeight = height;
-            Console.WindowWidth = width;
-            Console.CursorVisible = false;
-        }
-
-        public void Clear() => Console.Clear();
-
-        public void DrawBorder()
-        {
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            for (int i = 0; i < _width; i++)
-            {
-                Console.SetCursorPosition(i, 0);
-                Console.Write("■");
-                Console.SetCursorPosition(i, _height - 1);
-                Console.Write("■");
-            }
-            for (int i = 0; i < _height; i++)
-            {
-                Console.SetCursorPosition(0, i);
-                Console.Write("■");
-                Console.SetCursorPosition(_width - 1, i);
-                Console.Write("■");
-            }
-        }
-
-        public void DrawPixel(Pixel pixel)
-        {
-            Console.SetCursorPosition(pixel.X, pixel.Y);
-            Console.ForegroundColor = pixel.Color;
+            Console.SetCursorPosition(XPos, YPos);
+            Console.ForegroundColor = Color;
             Console.Write("■");
         }
-
-        public void DrawSnake(List<Pixel> body, ref bool gameover, Pixel head)
-        {
-            foreach (var segment in body)
-            {
-                DrawPixel(segment);
-                if (segment.X == head.X && segment.Y == head.Y)
-                {
-                    gameover = true;
-                }
-            }
-        }
-
-        public void DisplayGameOver(int score)
-        {
-            Console.SetCursorPosition(_width / 5, _height / 2);
-            Console.WriteLine($"Game Over! Score: {score}");
-            Console.ReadKey();
-        }
     }
 
-    interface InputHandler
+    struct Pixel
     {
-        Direction ReadMovement(Direction currentDirection);
-    }
+        public int X { get; }
+        public int Y { get; }
+        public ConsoleColor Color { get; }
 
-    class ConsoleInputHandler : InputHandler
-    {
-        public Direction ReadMovement(Direction currentDirection)
+        public Pixel(int x, int y, ConsoleColor color)
         {
-            if (Console.KeyAvailable)
-            {
-                var key = Console.ReadKey(true).Key;
-                return key switch
-                {
-                    ConsoleKey.UpArrow when currentDirection != Direction.Down => Direction.Up,
-                    ConsoleKey.DownArrow when currentDirection != Direction.Up => Direction.Down,
-                    ConsoleKey.LeftArrow when currentDirection != Direction.Right => Direction.Left,
-                    ConsoleKey.RightArrow when currentDirection != Direction.Left => Direction.Right,
-                    _ => currentDirection
-                };
-            }
-            return currentDirection;
+            X = x;
+            Y = y;
+            Color = color;
         }
     }
 
-    enum Direction { Up, Down, Left, Right }
-
-    struct Pixel { public int X, Y; public ConsoleColor Color; public Pixel(int x, int y, ConsoleColor color) { X = x; Y = y; Color = color; } }
+    enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
 }
